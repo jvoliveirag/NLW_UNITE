c652264ebb02fc909725179cf00bfef1b5dccc6e
jvoliveirag <jv.oliveira@gmail.com>
2024-04-03 18:12:23 -0300

    feat: Aula 3 - consumindo API

--- frontend/src/components/attendee-list.tsx
+++ frontend/src/components/attendee-list.tsx
@@ -1,27 +1,127 @@
+import dayjs from "dayjs";
+import "dayjs/locale/pt-br";
+import relativeTime from "dayjs/plugin/relativeTime";
 import { ChevronLeft, ChevronRight, ChevronsLeft, ChevronsRight, MoreHorizontal, Search } from "lucide-react";
+import { ChangeEvent, useEffect, useState } from "react";
+import { Attendee } from "../dtos/AttendeeDTO";
 import { IconButton } from "./icon-button";
 import { Table } from "./table/table";
 import { TableCell } from "./table/table-cell";
 import { TableHeader } from "./table/table-header";
 import { TableRow } from "./table/table-row";
 
-export function AttendeeList() {
-  return (
+dayjs.extend(relativeTime);
+dayjs.locale("pt-br");
 
+export function AttendeeList() {
+
+  const [search, setSearch] = useState(() => {
+    const url = new URL(window.location.toString());
+
+    if (url.searchParams.has("search")) {
+      return url.searchParams.get("search") ?? "";
+    }
+
+    return "";
+  });
+
+  const [page, setPage] = useState(() => {
+    const url = new URL(window.location.toString());
+
+    if (url.searchParams.has("page")) {
+      return Number(url.searchParams.get("page"));
+    }
+
+    return 1;
+  });
+
+  const [total, setTotal] = useState(0);
+  const [attendees, setAttendees] = useState<Attendee[]>([]);
+
+  const totalPages = Math.ceil(total / 10);
+  
+  useEffect(() => {
+    const url = new URL(
+      "http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees"
+    );
+
+    url.searchParams.set("pageIndex", String(page - 1));
+    if (search.length > 1) {
+      url.searchParams.set("query", search);
+    }
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setAttendees(data.attendees);
+        setTotal(data.total);
+      });
+  }, [page, search]); //sempre que mudar "page" ou "search" vai executar
+
+  function setCurrentSearch(search: string) {
+    const url = new URL(window.location.toString());
+
+    url.searchParams.set("search", search);
+
+    window.history.pushState({}, "", url);
+
+    setSearch(search);
+  }
+
+  function setCurrentPage(page: number) {
+    const url = new URL(window.location.toString());
+
+    url.searchParams.set("page", String(page));
+
+    window.history.pushState({}, "", url);
+
+    setPage(page);
+  }
+
+  function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>) {
+    setCurrentSearch(event.target.value);
+    setCurrentPage(1);
+  }
+
+  function goToFirstPage() {
+    setCurrentPage(1);
+  }
+
+  function goToLastPage() {
+    setCurrentPage(totalPages);
+  }
+
+  function goToPreviousPage() {
+    setCurrentPage(page - 1);
+  }
+
+  function goToNextPage() {
+    setCurrentPage(page + 1);
+  }
+
+  return (
     <div className="flex flex-col gap-4">
       <div className="flex gap-3 items-center">
         <h1 className="text-2xl font-bold">Participantes</h1>
-        <div className="w-72 px-3 py-1.5 border border-white/10 rounded-lg text-sm flex items-center gap-3">
-          <Search className="size-4" />
-          <input className="bg-transparent flex-1 outline-none p-0 border-0 text-sm" type="text" placeholder="Buscar participante..." />
+        <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg flex items-center gap-3">
+          <Search className="size-4 text-emerald-300" />
+          <input
+            className="bg-transparent focus:ring-0 flex-1 outline-none border-0 p-0 text-sm"
+            placeholder="Buscar participante..."
+            value={search}
+            onChange={onSearchInputChanged}
+          />
         </div>
       </div>
 
-      <Table className="w-full">
-        <thead className="">
+      <Table>
+        <thead>
           <tr className="border-b border-white/10">
-            <TableHeader style={{ width: 64 }}>
-              <input type="checkbox" className="size-4 bg-black/20 rounded border border-white/10" />
+            <TableHeader style={{ width: 48 }}>
+              <input
+                type="checkbox"
+                className="size-4 bg-black/20 rounded border border-white/10"
+              />
             </TableHeader>
             <TableHeader>Código</TableHeader>
             <TableHeader>Participante</TableHeader>
@@ -31,49 +131,72 @@ export function AttendeeList() {
           </tr>
         </thead>
         <tbody>
-          {Array.from({ length: 8 }).map((_, i) => {
+          {attendees.map((ateendee) => {
             return (
-              <TableRow key={i}>
+              <TableRow key={ateendee.id}>
                 <TableCell>
-                  <input type="checkbox" className="size-4 bg-black/20 rounded border border-white/10" />
+                  <input
+                    type="checkbox"
+                    className="size-4 bg-black/20 rounded border border-white/10"
+                  />
                 </TableCell>
-                <TableCell>12345</TableCell>
+                <TableCell>{ateendee.id}</TableCell>
                 <TableCell>
                   <div className="flex flex-col gap-1">
-                    <span className="font-semibold text-white">João Victor Oliveira</span>
-                    <span>jv.oliveirag@gmail.com</span>
+                    <span className="font-semibold text-white">
+                      {ateendee.name}
+                    </span>
+                    <span>{ateendee.email}</span>
                   </div>
                 </TableCell>
-                <TableCell>7 dias atrás</TableCell>
-                <TableCell>3 dias atrás</TableCell>
+                <TableCell>{dayjs().to(ateendee.createdAt)}</TableCell>
                 <TableCell>
-                  <IconButton transparent>
+                  {ateendee.checkedInAt === null ? (
+                    <span className="text-zinc-400">Não fez check-in</span>
+                  ) : (
+                    dayjs().to(ateendee.checkedInAt)
+                  )}
+                </TableCell>
+                <TableCell>
+                  <IconButton
+                    transparent
+                    className="bg-black/20 border border-white/10 rounded-md p-1.5"
+                  >
                     <MoreHorizontal className="size-4" />
                   </IconButton>
                 </TableCell>
               </TableRow>
-            )
+            );
           })}
         </tbody>
         <tfoot>
           <tr>
             <TableCell colSpan={3}>
-              Mostrando 8 de 228 intens
+              Mostrando {attendees.length} de {total} itens
             </TableCell>
-            <TableCell colSpan={3} className="py-3 px-4 text-sm text-zinc-300 text-right">
+            <TableCell className="text-right" colSpan={3}>
               <div className="inline-flex items-center gap-8">
-                <span>Página 1 de 23</span>
+                <span>
+                  Página {page} de {totalPages}
+                </span>
+
                 <div className="flex gap-1.5">
-                  <IconButton>
+                  <IconButton onClick={goToFirstPage} disabled={page === 1}>
                     <ChevronsLeft className="size-4" />
                   </IconButton>
-                  <IconButton>
+                  <IconButton onClick={goToPreviousPage} disabled={page === 1}>
                     <ChevronLeft className="size-4" />
                   </IconButton>
-                  <IconButton>
+                  <IconButton
+                    onClick={goToNextPage}
+                    disabled={page === totalPages}
+                  >
                     <ChevronRight className="size-4" />
                   </IconButton>
-                  <IconButton>
+                  <IconButton
+                    onClick={goToLastPage}
+                    disabled={page === totalPages}
+                  >
                     <ChevronsRight className="size-4" />
                   </IconButton>
                 </div>
